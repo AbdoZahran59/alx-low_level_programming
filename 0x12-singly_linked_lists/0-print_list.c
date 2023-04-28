d95bf9b82364bb06b7377acbf21126a089442202
Abdelrahman Zahran <81929007+AbdoZahran59@users.noreply.github.com>
2023-04-28 17:29:26 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,11 +3,6 @@
 #include <stdio.h>
 #include "lists.h"
 
-/**
- * print_list - check the code for Holberton School students.
- * @h: name of the list
- * Return: the number of nodes.
- */
 size_t print_list(const list_t *h)
 {
 	int count = 0;
