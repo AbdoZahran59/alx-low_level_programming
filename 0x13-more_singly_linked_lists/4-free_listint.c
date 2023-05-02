800ce78e21950458d21af246bd38f62ebf918356
Abdelrahman Zahran <81929007+AbdoZahran59@users.noreply.github.com>
2023-05-02 20:23:13 +0300

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,14 +1,5 @@
 #include "lists.h"
 #include <stdlib.h>
-
-/**
- * free_listint - frees a listint_t list
- *
- * @head: the head of the list
- *
- * Return: NULL if error
- */
-
 void free_listint(listint_t *head)
 {
 	listint_t *next;
